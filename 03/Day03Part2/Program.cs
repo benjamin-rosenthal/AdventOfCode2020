117620ff19282cd2148a1234d075ceb125072a0e
Benjamin Rosenthal <brosen8690@gmail.com>
2020-12-03 21:41:04 -0500

    gitignore'd stuff

---
+++ 03/Day03Part2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day03Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
