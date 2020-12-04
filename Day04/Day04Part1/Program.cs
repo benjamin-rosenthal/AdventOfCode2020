e798967525e5e00f4c744b76c9dd6df7ffdff62e
Benjamin Rosenthal <brosen8690@gmail.com>
2020-12-04 18:30:12 -0500

    solved day04part1

---
+++ Day04/Day04Part1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Day04Part1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputArray = System.IO.File.ReadAllText(@"../../../../input.txt").Split("\n\n");
+
+            int validPassports = 0;
+            foreach (var passwordRecord in inputArray)
+            {
+                if (IsPassportValid(passwordRecord)) validPassports++;
+            }
+
+            Console.WriteLine("Number of valid passports: " + validPassports);
+        }
+
+        public static bool IsPassportValid(string passportRecord)
+        {
+            if (passportRecord.Contains("byr:") &&
+                passportRecord.Contains("iyr:") &&
+                passportRecord.Contains("eyr:") &&
+                passportRecord.Contains("hgt:") &&
+                passportRecord.Contains("hcl:") &&
+                passportRecord.Contains("ecl:") &&
+                passportRecord.Contains("pid:"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
