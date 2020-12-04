d9747a2b9b5261dad54c7aef1125680ff21f308a
Benjamin Rosenthal <brosen8690@gmail.com>
2020-12-04 16:49:55 -0500

    Solved day3part1

--- Day03/Day03Part1/Program.cs
+++ Day03/Day03Part1/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day03Part1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputArray = System.IO.File.ReadAllText(@"../../../../input.txt").Split("\n");
+            char[,] inputGrid = new char[inputArray.Length, inputArray[0].Length];
+            for (int rowCount = 0; rowCount < inputGrid.GetLength(0); rowCount++)
+            {
+                for (int columnCount = 0; columnCount < inputGrid.GetLength(1); columnCount++)
+                {
+                    if (!string.IsNullOrWhiteSpace(inputArray[rowCount].ToString()))
+                    {
+                        var charToGet = inputArray[rowCount].ToCharArray()[columnCount];
+                        inputGrid[rowCount, columnCount] = charToGet;
+                    }
+                }
+            }
+
+            char[,] expandedGrid = new char[inputGrid.GetLength(0), (inputGrid.GetLength(0) * 3)];
+            for (int rowCount = 0; rowCount < expandedGrid.GetLength(0); rowCount++)
+            {
+                for (int columnCount = 0; columnCount < expandedGrid.GetLength(1); columnCount++)
+                {
+                    //ALL HAIL MODULUS
+                    expandedGrid[rowCount, columnCount] = inputGrid[rowCount,(columnCount % inputGrid.GetLength(1))];
+                }
+            }
+
+
+            int columnIndex = 0;
+            int rowIndex = 0;
+            int treeCount = 0;
+            do
+            {
+                if (expandedGrid[rowIndex, columnIndex] == '#') treeCount++;
+                rowIndex++;
+                columnIndex += 3;
+            }
+            while (rowIndex < expandedGrid.GetLength(0));
+
+            Console.WriteLine("Augh! I ran into " + treeCount + " trees!");
         }
     }
 }
