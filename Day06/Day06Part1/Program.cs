3d842045305c8719d3c0552a4b7bd74eebd1f897
Benjamin Rosenthal <brosen8690@gmail.com>
2020-12-06 09:03:12 -0500

    Solved day06part1

---
+++ Day06/Day06Part1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day06Part1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Wonder if I could make this a one liner somehow
+            int answerCount = 0;
+            System.IO.File.ReadAllText(@"../../../../input.txt").Split("\n\n").Select(x => x.Replace("\n", "")).ToList().ForEach(x => answerCount += x.ToCharArray().Distinct().Count());
+
+            Console.WriteLine("There are " + answerCount + " distinct answers");
+        }
+    }
+}
