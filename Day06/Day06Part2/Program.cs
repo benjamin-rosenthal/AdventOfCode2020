72dc2643a27f01e414ae55c1c80a890c47a354fc
Benjamin Rosenthal <brosen8690@gmail.com>
2020-12-06 10:03:53 -0500

    Not understanding why this doesn't work, so I'm going to take a break here

--- Day06/Day06Part2/Program.cs
+++ Day06/Day06Part2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day06Part2
 {
@@ -6,7 +8,53 @@ namespace Day06Part2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = System.IO.File.ReadAllText(@"../../../../input.txt").Split("\n\n");
+
+            int numberOfQuestionsThatEveryoneAnsweredYesTo = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] answersByPerson = input[i].Split("\n");
+                answersByPerson = answersByPerson.OrderByDescending(x => x.Length).ToArray();
+
+                List<char> sharedAnswers = answersByPerson[0].ToCharArray().ToList();
+                for (int j = 0; j < answersByPerson.Length; j++)
+                {
+                    List<char> currentPersonAnswers = answersByPerson[j].ToCharArray().ToList();
+                    sharedAnswers = sharedAnswers.Intersect(currentPersonAnswers.Select(x => x)).ToList();
+                }
+                numberOfQuestionsThatEveryoneAnsweredYesTo += sharedAnswers.Count;
+            }
+
+            Console.WriteLine("There are " + numberOfQuestionsThatEveryoneAnsweredYesTo + " questions that everyone answered yes to.");
         }
+
+        //static void Main(string[] args)
+        //{
+        //    string[] input = new string[5];
+        //    input[0] = "abc";
+        //    input[1] = "a\nb\nc";
+        //    input[2] = "ab\nac";
+        //    input[3] = "a\na\na\na";
+        //    input[4] = "b";
+
+        //    int numberOfQuestionsThatEveryoneAnsweredYesTo = 0;
+        //    for (int i = 0; i < input.Length; i++)
+        //    {
+        //        string[] answersByPerson = input[i].Split("\n");
+        //        answersByPerson = answersByPerson.OrderByDescending(x => x.Length).ToArray();
+
+        //        List<char> sharedAnswers = answersByPerson[0].ToCharArray().ToList();
+        //        for (int j = 0; j < answersByPerson.Length; j++)
+        //        {
+        //            List<char> currentPersonAnswers = answersByPerson[j].ToCharArray().ToList();
+        //            sharedAnswers = sharedAnswers.Intersect(currentPersonAnswers.Select(x => x)).ToList();
+        //        }
+        //        Console.WriteLine("Everyone answered yes to " + sharedAnswers.Count() + " questions");
+        //        numberOfQuestionsThatEveryoneAnsweredYesTo += sharedAnswers.Count;
+
+        //    }
+
+        //    Console.WriteLine("There are " + numberOfQuestionsThatEveryoneAnsweredYesTo + " questions that everyone answered yes to.");
+        //}
     }
 }
